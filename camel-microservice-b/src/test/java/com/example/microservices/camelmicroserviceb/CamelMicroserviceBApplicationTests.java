fcd21c6ed6cbef42b93f76151fa5d393004fea89
Chaitanya U K <uk.chaitanya121@gmail.com>
2021-09-20 17:26:38 +0530

    first commit

---
+++ camel-microservice-b/src/test/java/com/example/microservices/camelmicroserviceb/CamelMicroserviceBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.microservices.camelmicroserviceb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamelMicroserviceBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
